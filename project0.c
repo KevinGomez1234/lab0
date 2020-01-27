944177005f17f04da0de1d8c4ac9f28ebb4e116c
Kevin Gomez <kevin_gomez@computer.local>
2020-01-27 12:05:16 -0800

    blueprint and logic

--- project0.c
+++ project0.c
@@ -5,3 +5,23 @@ int main()
 	printf("Hello World");
 	return 0;
 }
+
+void readBytes(){
+int fileDesc;
+char charBuffer;
+
+fd = open("filename.txt", O_RDONLY);
+
+	
+
+}
+
+void logic ()
+{
+	printf("Original\tASCII\tDecimal\tParity");
+	printf("\n--------\t--------\t--------\t--------");
+	//decide whether reading from file or stdout
+	//if reading from stdin then decode that
+	//else reading from the file and decode
+	//done:
+}
