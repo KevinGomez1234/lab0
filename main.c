d995a7be8b2b1e85d80c1387c6d10bb03744c3dd
Kevin Gomez <kevin_gomez@computer.local>
2020-02-15 13:00:11 -0800

    Added comments and fixed indentation

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ void logic(int argc, char* argv []);
 void printInformation(int file_descriptor, int terminator);
 void printArgsInformation(int argc, char* argv[]);
 
+//Non-printable ascii characters 0-32 && 127
 static const char nonPrintables [34][6] = 
 {
 	"NUL  ", "SOH  ", "STX  ", "ETX  ", "EOT  ", "ENQ  ", "ACK  ", "BEL  ", 
@@ -18,13 +19,13 @@ static const char nonPrintables [34][6] =
 };
 
 
-
 int main(int argc,char* argv [])
 {
 	logic(argc,argv);
 	return 0;
 }
 
+//Prints out information if we're reading from a file.
 void printInformation(int file_descriptor, int terminator)
 {
 	int decimal = 0;
@@ -36,42 +37,45 @@ void printInformation(int file_descriptor, int terminator)
 	const char** ptr;
 	printf("Original        ASCII        Decimal        Parity \n");
 	printf("-------        -------        -------        ------- \n");
-
-	//use read(fd, buff, how many bits u want read)
+	//Read from a file using read()
 	while(read(file_descriptor, &ch[0], sizeof(ch)) > 0 && ch[0] != terminator)
 	{
-		//if a 0 or 1 is read then do something
+		//Only consider 1's or 0's for the input and and don't run this if we've read all 8 characters
 		if(((int)ch[0] == 49 ||(int)ch[0] == 48) && counter != -1 )
 		{	
+			//atoi() converts a string to an integer easily or we could do (int)ch[0]%48, but atoi() is more readable.  
 			printf("%d", atoi(&ch[0]));
 			if (atoi(&ch[0]) == 1)
 				parityChecker++;
 			if(counter!=7)
+				//atoi returns the actual value of ch[0] which is either a 1 or a 0.
 				decimal = (pow(2, counter) * atoi(&ch[0])) + decimal;
 			counter--;
 		}//endwhile
-		
+			
 		//we've read 8 chars therefore print the missing information out
 		if (counter == -1)
 		{
+			//check parity of binary string and point to correct string (odd or even).
 			if(parityChecker % 2 == 0)
 				ptr = &EVEN;
 			else
 				ptr = &ODD;
-			//edge cases
+			//edge cases to print out 0-32 or 127
 			if(decimal>=0 && decimal<=32)
 					printf("\t  %s         %d\t\t%s\n", nonPrintables[decimal],decimal, *ptr);
 			else if(decimal == 127)
 					printf("\t  %s         %d\t\t%s\n", nonPrintables[33],decimal, *ptr);
 			else	
 					printf("\t  %c             %d\t\t%s\n", decimal,decimal, *ptr);
+			//reset vars
 			counter = 7;
 			decimal = 0;
 			parityChecker = 0;
 		}
 	}
 
-	//if counter has not ended, but we reached end of the loop, then we have extra 0's to pad.
+	//if counter has not ended and we havent started reading new chars, but we reached end of the loop, then we have extra 0's to pad.
 	if(counter != -1 && counter != 7)
 	{
 			if(parityChecker % 2 == 0)
@@ -95,6 +99,7 @@ void printInformation(int file_descriptor, int terminator)
 	}
 }
 
+//reading the args with a for loop. 
 void printArgsInformation(int argc, char* argv[])
 {
 	int arraySize;
@@ -107,9 +112,10 @@ void printArgsInformation(int argc, char* argv[])
 	char ch[1];
 	printf("Original        ASCII        Decimal        Parity \n");
 	printf("-------        -------        -------        ------- \n");
-
+	//read argv[] starting from 1. 
 	for(int i = 1; i<argc;i++)
 	{
+		//the length of the array is strlen or until argv[i][n] == \0. 
 		arraySize = strlen(argv[i]); 
 		for(int j = 0; j<arraySize;j++)
 		{
@@ -120,6 +126,7 @@ void printArgsInformation(int argc, char* argv[])
 				if(atoi(&ch[0]) == 1)
 					parityChecker++;
 				if(counter != 7)
+					//atoi returns the actual value of ch[0] which is either a 1 or a 0.
 					decimal = (pow(2, counter) * atoi(&ch[0])) + decimal;
 				counter--;
 			}
@@ -130,7 +137,6 @@ void printArgsInformation(int argc, char* argv[])
 					ptr = &EVEN;
 				else
 					ptr = &ODD;
-
 				//edge cases
 				if(decimal>=0 && decimal<=32)
 					printf("\t  %s         %d\t\t%s\n", nonPrintables[decimal],decimal, *ptr);
@@ -145,17 +151,19 @@ void printArgsInformation(int argc, char* argv[])
 		}
 	}
 
+	// pad extra 0's
 	if(counter != -1 && counter != 7)
 	{
 			if(parityChecker % 2 == 0)
 				ptr = &EVEN;
 			else
 				ptr = &ODD;
+			//print out missing 0's
 			for(int i = 0;i < counter + 1;i++)
 			{
 				printf("0");
 			}
-			//edge cases
+			//edge cases 
 			if(decimal>=0 && decimal<=32)
 					printf("\t  %s         %d\t\t%s\n", nonPrintables[decimal],decimal, *ptr);
 			else if(decimal == 127)
@@ -168,8 +176,10 @@ void printArgsInformation(int argc, char* argv[])
 	}
 }
 
+//Logic for the program
 void logic (int argc,char* argv [])
 {
+	//if no input is passed there is an error
 	if(argc == 1)
 		printf("Invalid input program will now exit.\n");
 	else if(argc >= 2)
@@ -180,14 +190,14 @@ void logic (int argc,char* argv [])
 		if(fd == -1)
 		{
 			close(fd);
-			if( (argv[1][0] == '1' || argv[1][0] == '0'  || *argv[1] == '-') )
+			//if the second argument doesnt begin with 1 or 0 or - then it's probably a mistake...
+			if(*argv[1] == '-' || argv[1][0] == '1' || argv[1][0] == '0')
 				printArgsInformation(argc, argv);
-			//not any then it's invalid input
+			//else it's not valid input
 			else 
 				printf("Invalid input, program will now exit\n");
 		}
-
-		//if fd != -1 then it's a proper file descriptor and read file
+		//if it is valid file then open and read it with printInformation();
 		else
 		{
 			int fd = open(argv[1], O_RDONLY);
